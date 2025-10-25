54d0e894b4e360eb9ed379f2e71c7ef9d2a1853c
Shumail-Gul <shumailgul140@gmail.com>
2025-10-25 20:14:34 +0500

    Clean up Create_Database.sql by removing queries
    Removed multiple SELECT statements and a VIEW creation from the SQL script.

--- Create_Database.sql
+++ Create_Database.sql
@@ -16,48 +16,7 @@ CREATE TABLE SuperMarketSale (
 );
  
  SELECT 
-    COUNT(*)
+*
 FROM
     SuperMarketSale;
 
-SELECT 
-    product_line, SUM(sales) AS total_Sales
-FROM
-    SuperMarketSale
-GROUP BY product_line
-ORDER BY total_Sales DESC;
-
-SELECT 
-    city, round(AVG(rating), 2) AS Average_Rating
-FROM
-    SuperMarketSale
-GROUP BY city
-ORDER BY Average_Rating DESC;
-
-SELECT 
-    Payment, COUNT(*) AS top_Payment_Methods
-FROM
-    SuperMarketSale
-GROUP BY Payment
-ORDER BY top_Payment_Methods DESC
-LIMIT 3;
-
-select gender, sum(sales) from SuperMarketSale
-group by gender;
-
-SELECT 
-    Product_line, ROUND(AVG(quantity), 2) AS Average_Quantity
-FROM
-    SuperMarketSale
-GROUP BY Product_line
-ORDER BY Average_Quantity DESC;
-
-select date, sum(Sales) as Total_Sales from SuperMarketSale
-group by date order by Total_Sales Desc;
-
-CREATE VIEW Sale_Summary AS
-    SELECT 
-        branch, city, SUM(sales) AS total_Sales
-    FROM
-        SuperMarketSale
-    GROUP BY branch , city;
